7a943f63f84386002e745f8b31f0d82019bffebd
Kevin <krmartinezs28@gmail.com>
2026-06-26 21:49:14 -0600

    fix(devops): implementar endpoints

--- src/app.py
+++ src/app.py
@@ -1,16 +1,27 @@
 from flask import Flask, jsonify
 import os
 
+# Inicializa la app Flask
 app = Flask(__name__)
 
 @app.route('/health', methods=['GET'])
 def health():
+    """
+    Ruta de comprobacion de estado Healthcheck
+    Devuelve un estado 'ok' para indicar que el servidor esta en línea.
+    """
     return jsonify({"status": "ok"}), 200
 
 @app.route('/version', methods=['GET'])
 def version():
+    """
+    Ruta para obtener la versioon de la app
+    Lee la variable de entorno 'APP_VERSION'.
+    """
+    # Si no se encuentra la variable de entorno, usa '1.0.0' por defecto
     app_version = os.getenv('APP_VERSION', '1.0.0')
     return jsonify({"version": app_version}), 200
 
 if __name__ == '__main__':
+    # Ejecuta el servidor en todas las interfaces (0.0.0.0) a traves del puerto 8080
     app.run(host='0.0.0.0', port=8080, debug=False)
